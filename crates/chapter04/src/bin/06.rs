a531e73863d4492ac3368bd1938da5b9a967d728
sammyne <xiangminli-li@qq.com>
2025-07-04 14:00:23 +0800

    feat(chapter04): done '4.6 Coding the GPT model'

--- crates/chapter04/src/bin/06.rs
+++ crates/chapter04/src/bin/06.rs
@@ -1,25 +1,23 @@
 use burn::backend::{Autodiff, NdArray};
 use burn::prelude::Backend;
 use burn::tensor::{Distribution, Tensor};
-use chapter04::{TransformerBlock, GPT_124M};
+use chapter04::{GPT_124M, TransformerBlock};
 
 type B = Autodiff<NdArray<f32>>;
 
 fn main() -> anyhow::Result<()> {
-  B::seed(123);
+    B::seed(123);
 
-  let device = <B as Backend>::Device::default();
+    let device = <B as Backend>::Device::default();
 
-  let x = Tensor::<B,3>::random([2,4,768], Distribution::Uniform(0.0, 1.0), &device);
+    let x = Tensor::<B, 3>::random([2, 4, 768], Distribution::Uniform(0.0, 1.0), &device);
 
-  let block = TransformerBlock::<B>::new(&GPT_124M);
+    let block = TransformerBlock::<B>::new(&GPT_124M);
 
-  let output= block.forward(x.clone());
+    let output = block.forward(x.clone());
 
-  println!("Input shape: {:?}", x.shape());
-  println!("Output shape: {:?}", output.shape());
+    println!("Input shape: {:?}", x.shape());
+    println!("Output shape: {:?}", output.shape());
 
-
-
-  Ok(())
+    Ok(())
 }
