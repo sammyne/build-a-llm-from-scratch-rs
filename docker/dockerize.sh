c17ade0efaa1e480ccaf21fd498600761f589a55
sammyne <xiangmin-li@qq.com>
2025-08-23 07:28:46 +0000

    feat(docker): upgrade to match Colab at 20250823

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -2,6 +2,26 @@
 
 cd `dirname ${BASH_SOURCE[0]}`
 
-build_arg_rust_toolchain="--build-arg RUST_TOOLCHAIN=1.89.0"
+# 和 2025-08-23 的 Colab 保持一致
+cuda=12.6.3
+pytorch=2.8.0+cu`echo $cuda | awk -F'.' '{print $1 $2}'`
+ubuntu=22.04
+python=3.12
 
-docker build $build_arg_rust_toolchaino -t sammyne/build-a-llm-from-scratch-rs:alpha .
+build_arg_cuda="--build-arg CUDA_VERSION=$cuda"
+build_arg_python="--build-arg PYTHON_VERSION=$python"
+build_arg_pytorch="--build-arg PYTORCH_VERSION=$pytorch"
+build_arg_rust_toolchain="--build-arg RUST_TOOLCHAIN=1.89.0"
+build_arg_ubuntu="--build-arg UBUNTU_VERSION=$ubuntu"
+
+build_arg_opts=$build_arg_cuda
+build_arg_opts="$build_arg_opts $build_arg_python"
+build_arg_opts="$build_arg_opts $build_arg_pytorch"
+build_arg_opts="$build_arg_opts $build_arg_rust_toolchain"
+build_arg_opts="$build_arg_opts $build_arg_ubuntu"
+
+tag=colab20250823
+
+echo $pytorch | awk -F'+' '{print $2}'
+
+docker build $build_arg_opts -t sammyne/build-a-llm-from-scratch-rs:`git rev-parse --short HEAD`-$tag .
