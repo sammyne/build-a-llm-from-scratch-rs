aa5a9c63546c01af20ddac235b9305538889d7e5
sammyne <xiangmin-li@qq.com>
2025-09-16 12:48:48 +0000

    refactor(docker): separate out google-colab part

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -2,26 +2,12 @@
 
 cd `dirname ${BASH_SOURCE[0]}`
 
-# 和 2025-08-23 的 Colab 保持一致
-cuda=12.6.3
-pytorch=2.8.0+cu`echo $cuda | awk -F'.' '{print $1 $2}'`
-ubuntu=22.04
-python=3.12
+googleclab_rev=20250803
 
-build_arg_cuda="--build-arg CUDA_VERSION=$cuda"
-build_arg_python="--build-arg PYTHON_VERSION=$python"
-build_arg_pytorch="--build-arg PYTORCH_VERSION=$pytorch"
+build_arg_google_colab="--build-arg GOOGLE_COLAB_VERSION=20250803-ubuntu22.04"
 build_arg_rust_toolchain="--build-arg RUST_TOOLCHAIN=1.89.0"
-build_arg_ubuntu="--build-arg UBUNTU_VERSION=$ubuntu"
 
-build_arg_opts=$build_arg_cuda
-build_arg_opts="$build_arg_opts $build_arg_python"
-build_arg_opts="$build_arg_opts $build_arg_pytorch"
+build_arg_opts=$build_arg_google_colab
 build_arg_opts="$build_arg_opts $build_arg_rust_toolchain"
-build_arg_opts="$build_arg_opts $build_arg_ubuntu"
 
-tag=colab20250823
-
-echo $pytorch | awk -F'+' '{print $2}'
-
-docker build $build_arg_opts -t sammyne/build-a-llm-from-scratch-rs:`git rev-parse --short HEAD`-$tag .
+docker build $build_arg_opts -t sammyne/build-a-llm-from-scratch-rs:`git rev-parse --short HEAD` .
